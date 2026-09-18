9cfe9b4e13ffa48e4d6087e2a6ae4d63f278f065
Nox Compiler Project <dev@nox-lang.local>
2026-09-18 20:34:55 +0000

    parser: parser.go: add Parse (5/20)

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -15,3 +15,23 @@ type Parser struct {
 	filename string
 }
 
+func Parse(src, filename string) (file *ast.File, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			switch v := r.(type) {
+			case parseError:
+				err = fmt.Errorf("%s", string(v))
+			case string:
+				err = fmt.Errorf("%s", v)
+			case error:
+				err = v
+			default:
+				panic(r)
+			}
+		}
+	}()
+	toks := lexer.Tokenize(src, filename)
+	p := &Parser{toks: toks, pos: 0, filename: filename}
+	return p.parseFile(), nil
+}
+
