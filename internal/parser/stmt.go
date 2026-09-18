6aa312b8ffec3d9e0831164e299e0cb6643a0e58
Nox Compiler Project <dev@nox-lang.local>
2026-09-18 20:34:55 +0000

    parser: stmt.go: add Parser.parseBlock (3/13)

--- internal/parser/stmt.go
+++ internal/parser/stmt.go
@@ -5,3 +5,13 @@ import (
 	"nox/internal/token"
 )
 
+func (p *Parser) parseBlock() *ast.BlockStmt {
+	lb := p.expect(token.LBRACE)
+	b := &ast.BlockStmt{Base: ast.NewBase(lb.Line, lb.Col)}
+	for !p.at(token.RBRACE) {
+		b.Stmts = append(b.Stmts, p.parseStmt())
+	}
+	p.expect(token.RBRACE)
+	return b
+}
+
