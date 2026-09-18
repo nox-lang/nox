080e3d4504602a37791946489db4446b2c502fd4
Nox Compiler Project <dev@nox-lang.local>
2026-09-18 20:34:57 +0000

    codegen: builtin_methods.go: add var builtinMethodNames (4/20)

--- internal/codegen/builtin_methods.go
+++ internal/codegen/builtin_methods.go
@@ -7,3 +7,11 @@ import (
 	"nox/internal/ast"
 )
 
+var builtinMethodNames = map[string]bool{
+	"push": true, "pop": true, "insert": true, "remove": true, "clear": true,
+	"each": true, "eachIndex": true, "eachLine": true,
+	"map": true, "filter": true, "find": true, "sort": true, "reverse": true,
+	"contains": true, "startsWith": true, "endsWith": true, "substring": true, "empty": true,
+	"toInt": true, "toFloat": true, "toBool": true, "toString": true,
+}
+
