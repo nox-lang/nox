75ecf956e631cf4b147ed26f9c261fde33545fc1
Nox Compiler Project <dev@nox-lang.local>
2026-09-18 20:34:55 +0000

    parser: stmt.go: add Parser.parseStmt (5/13)

--- internal/parser/stmt.go
+++ internal/parser/stmt.go
@@ -29,3 +29,49 @@ func (p *Parser) parseLetStmt() *ast.LetStmt {
 	return ls
 }
 
+func (p *Parser) parseStmt() ast.Stmt {
+	switch p.cur().Kind {
+	case token.LET, token.PRIVATE:
+		return p.parseLetStmt()
+	case token.IF:
+		return p.parseIfStmt()
+	case token.FOR:
+		return p.parseForStmt()
+	case token.WHILE:
+		return p.parseWhileStmt()
+	case token.BREAK:
+		bt := p.advance()
+		bs := &ast.BreakStmt{Base: ast.NewBase(bt.Line, bt.Col)}
+		if p.canStartExpr() {
+			bs.Value = p.parseExpr()
+		}
+		return bs
+	case token.NEXT:
+		nt := p.advance()
+		ns := &ast.NextStmt{Base: ast.NewBase(nt.Line, nt.Col)}
+		if p.canStartExpr() {
+			ns.Value = p.parseExpr()
+		}
+		return ns
+	case token.YIELD:
+		yt := p.advance()
+		return &ast.YieldStmt{Base: ast.NewBase(yt.Line, yt.Col), Value: p.parseExpr()}
+	case token.RETURN:
+		rt := p.advance()
+		rs := &ast.ReturnStmt{Base: ast.NewBase(rt.Line, rt.Col)}
+		if p.canStartExpr() {
+			rs.Value = p.parseExpr()
+		}
+		return rs
+	case token.SWITCH:
+		return p.parseSwitchStmt()
+	case token.DEFER:
+		dt := p.advance()
+		return &ast.DeferStmt{Base: ast.NewBase(dt.Line, dt.Col), Body: p.parseBlock()}
+	case token.TRY:
+		return p.parseTryStmt()
+	default:
+		return p.parseExprOrAssignStmt()
+	}
+}
+
