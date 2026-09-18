1279db19289838e014ba4e4ab6486570b1c673ac
Nox Compiler Project <dev@nox-lang.local>
2026-09-18 20:34:55 +0000

    parser: util.go: add parseIntLiteral (3/4)

--- internal/parser/util.go
+++ internal/parser/util.go
@@ -2,3 +2,16 @@ package parser
 
 import "strconv"
 
+func parseIntLiteral(s string) int64 {
+	v, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		// Fall back to unsigned parsing then reinterpret, for very large literals.
+		u, uerr := strconv.ParseUint(s, 10, 64)
+		if uerr == nil {
+			return int64(u)
+		}
+		return 0
+	}
+	return v
+}
+
