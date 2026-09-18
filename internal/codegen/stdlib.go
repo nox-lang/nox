3cd6678fcfa51e5370c5783bf3db21820d766edb
Nox Compiler Project <dev@nox-lang.local>
2026-09-18 20:34:57 +0000

    codegen: stdlib.go: add stdlibConstant (4/22)

--- internal/codegen/stdlib.go
+++ internal/codegen/stdlib.go
@@ -7,3 +7,15 @@ import (
 	"nox/internal/ast"
 )
 
+func stdlibConstant(pkg, sym string) (string, Type, bool) {
+	if pkg == "math" {
+		switch sym {
+		case "PI":
+			return "3.14159265358979323846", TFloat(), true
+		case "E":
+			return "2.71828182845904523536", TFloat(), true
+		}
+	}
+	return "", Type{}, false
+}
+
