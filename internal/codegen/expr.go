1c4ac60ff3090dff60daf7f289532fdefd132e8d
Nox Compiler Project <dev@nox-lang.local>
2026-09-18 20:34:56 +0000

    codegen: expr.go: add funcBuilder.genExpr (4/16)

--- internal/codegen/expr.go
+++ internal/codegen/expr.go
@@ -9,3 +9,70 @@ import (
 	"nox/internal/token"
 )
 
+func (fb *funcBuilder) genExpr(c *ctx, e ast.Expr) (string, Type) {
+	switch x := e.(type) {
+	case *ast.IntLit:
+		return fmt.Sprintf("%dLL", x.Value), TInt()
+	case *ast.FloatLit:
+		return formatFloatLiteral(x.Value), TFloat()
+	case *ast.StringLit:
+		return fmt.Sprintf(`nox_string_from_cstr(%s)`, cStringLiteral(x.Value)), TString()
+	case *ast.BoolLit:
+		if x.Value {
+			return "true", TBool()
+		}
+		return "false", TBool()
+	case *ast.NullLit:
+		panic("nox: explicit 'null' cannot appear in an expression; declare an uninitialized variable with 'let name: Type' instead")
+	case *ast.ThisExpr:
+		t, ok := c.scope.lookup("this")
+		if !ok {
+			panic("nox: 'this' used outside of a method")
+		}
+		return cIdent("this"), t
+	case *ast.Ident:
+		return fb.genIdent(c, x)
+	case *ast.QualIdent:
+		return fb.genQualIdentValue(c, x)
+	case *ast.ArrayLit:
+		return fb.genArrayLit(c, x)
+	case *ast.BinaryExpr:
+		return fb.genBinaryExpr(c, x)
+	case *ast.UnaryExpr:
+		return fb.genUnaryExpr(c, x)
+	case *ast.CallExpr:
+		return fb.genCallExpr(c, x)
+	case *ast.IndexExpr:
+		return fb.genIndexExpr(c, x)
+	case *ast.MemberExpr:
+		return fb.genMemberRead(c, x)
+	case *ast.FuncLit:
+		return fb.genFuncLitValue(c, x)
+	case *ast.PropagateExpr:
+		return fb.genPropagateExpr(c, x)
+	case *ast.AwaitExpr:
+		return fb.genAwaitExpr(c, x)
+	case *ast.ParallelExpr:
+		return fb.genParallelExpr(c, x)
+	case *ast.ForCondStmt:
+		code, t, vv := fb.genForCond(c.scope, x, true)
+		c.emit(code)
+		return vv, t
+	case *ast.ForInStmt:
+		code, t, vv := fb.genForIn(c.scope, x, true)
+		c.emit(code)
+		return vv, t
+	case *ast.WhileStmt:
+		code, t, vv := fb.genWhile(c.scope, x, true)
+		c.emit(code)
+		return vv, t
+	case *ast.SwitchStmt:
+		code, t, vv := fb.genSwitch(c.scope, x, true)
+		c.emit(code)
+		return vv, t
+	case *ast.IfStmt:
+		return fb.genIfExpr(c, x)
+	}
+	panic(fmt.Sprintf("codegen: unhandled expression %T", e))
+}
+
