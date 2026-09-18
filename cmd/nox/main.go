fc968c36e742dfe830beb27ea59c0bc6d42a19ba
Nox Compiler Project <dev@nox-lang.local>
2026-09-18 20:34:57 +0000

    cli: main.go: add main (4/18)

--- cmd/nox/main.go
+++ cmd/nox/main.go
@@ -18,3 +18,32 @@ import (
 	noxruntime "nox/internal/runtime"
 )
 
+func main() {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+	var err error
+	switch os.Args[1] {
+	case "init":
+		err = cmdInit(os.Args[2:])
+	case "build":
+		err = cmdBuild(os.Args[2:])
+	case "get":
+		err = cmdGet(os.Args[2:])
+	case "version", "-v", "--version":
+		fmt.Println("nox version 0.1.0")
+		return
+	case "help", "-h", "--help":
+		printUsage()
+		return
+	default:
+		printUsage()
+		os.Exit(1)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "nox: %s\n", err)
+		os.Exit(1)
+	}
+}
+
