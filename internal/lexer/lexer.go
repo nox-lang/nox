402202040bc351d14d61cb2f18e0e5f5e4fe5c21
Nox Compiler Project <dev@nox-lang.local>
2026-09-18 20:34:54 +0000

    lexer: lexer.go: add Lexer.peekCh (6/16)

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -20,3 +20,10 @@ func New(src, filename string) *Lexer {
 	return &Lexer{src: []rune(src), pos: 0, line: 1, col: 1, filename: filename}
 }
 
+func (l *Lexer) peekCh() rune {
+	if l.pos >= len(l.src) {
+		return 0
+	}
+	return l.src[l.pos]
+}
+
