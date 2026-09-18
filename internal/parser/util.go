ee17da7a22b5e128677af483ef7d7f54e5d440d5
Nox Compiler Project <dev@nox-lang.local>
2026-09-18 20:34:55 +0000

    parser: util.go: add package decl (1/4)

--- internal/parser/util.go
+++ internal/parser/util.go
@@ -1,24 +1,2 @@
 package parser
 
-import "strconv"
-
-func parseIntLiteral(s string) int64 {
-	v, err := strconv.ParseInt(s, 10, 64)
-	if err != nil {
-		// Fall back to unsigned parsing then reinterpret, for very large literals.
-		u, uerr := strconv.ParseUint(s, 10, 64)
-		if uerr == nil {
-			return int64(u)
-		}
-		return 0
-	}
-	return v
-}
-
-func parseFloatLiteral(s string) float64 {
-	v, err := strconv.ParseFloat(s, 64)
-	if err != nil {
-		return 0
-	}
-	return v
-}
